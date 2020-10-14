44086067db35941be01f006319202230d7ecc6d5
yuya.takahashi <Pochaty119@outlook.jp>
2020-10-14 10:29:52 +0000

    modified show

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -7,16 +7,16 @@
     <table class="table table-striped">
         <tr>
             <th>id</th>
-            <td>{{ $message->id }}</td>
+            <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
-            <td>{{ $message->content }}</td>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
         </tr>
     </table>
 
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
-    {!! Form::model($task, ['route' => ['task.destroy', $task->id],'method' => 'delete'] !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id],'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
